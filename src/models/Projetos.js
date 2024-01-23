93c8cf52cce4ef847a2a52000cf359f4d8aaab8f
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2024-01-23 19:24:10 -0300

    mudança de caminho

---
+++ src/models/Projetos.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const projetoSchema = new Schema(
+    {
+        usuario_id: {
+            type: String,
+            required: true,
+        },
+        titulo: {
+            type: String,
+            required: true,
+        },
+        tags: {
+            type: Array,
+            required: true,
+        },
+        link: {
+            type: String,
+            required: true,
+        },
+        descricao: {
+            type: String,
+            required: true,
+        },
+        imagem_url: {
+            type: String,
+            required: true,
+        },
+    },
+    { timestamps: true },
+);
+
+const Projeto = mongoose.model("Projeto", projetoSchema);
+
+module.exports = {
+    Projeto,
+    projetoSchema,
+};
