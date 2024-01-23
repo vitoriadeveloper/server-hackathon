93a47bf9438bdb1cd762f35ae07d66e26420ed9e
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2024-01-23 19:24:43 -0300

    implementação de middleware

---
+++ src/middleware/auth.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+const Usuario = require("../models/Usuarios");
+
+const autenticacao = async (req, res, next) => {
+    const { authorization } = req.headers;
+    try {
+        if (!authorization) {
+            return res.status(401).json({ message: "Não autorizado" });
+        }
+        const token = authorization.split("")[1];
+        const { id } = jwt.verify(token, process.env.SECURE_PASSWORD);
+        const usuarioLogado = await Usuario.findById({ id });
+        if (!usuarioLogado) {
+            return res.status(401).json({ message: "Usuário não autorizado" });
+        }
+        const { senha_hash: _, ...dadosUsuarioLogado } = usuarioLogado;
+        req.usuarioLogado = dadosUsuarioLogado;
+
+        next();
+    } catch (error) {
+        return res.status(500).json({
+            message: "Erro de autenticação",
+        });
+    }
+};
+module.exports = {
+    autenticacao,
+};
