6ab2d137fff4feb4fe678e742a513a7eccf482ae
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2024-01-25 12:34:11 -0300

    biblioteca cors adicionada

--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ const cors = require("cors");
 const app = express();
 const PORTA = 8000;
 
-app.use(cors());
+const corsOptions = {
+    origin: ["http://localhost:8000", "http://localhost:5173"],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.get("/", (req, res) => {
     res.send("Olá mundo");
