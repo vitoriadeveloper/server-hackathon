6a6caa9f9d9f2ca560d867569e25c6bec715df82
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2024-01-25 09:53:30 -0300

    cors implemented

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 require("dotenv").config();
+const cors = require("cors");
 const app = express();
 const PORTA = 8000;
 
+app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => {
     res.send("Olá mundo");
